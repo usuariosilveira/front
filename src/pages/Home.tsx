3ada168c0be2f1edc6b20585cf68bcdb9c24a277
usuariosilveira <moniquesilveiraa28@gmail.com>
2023-11-13 20:12:25 -0300

    novos componentes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+ function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opiniões</p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
+
